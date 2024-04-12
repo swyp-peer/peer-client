94144292a26e527d1ab078ae59cc8ede264f5c46
hyunsuk97 <mrbonk97@gmail.com>
2024-04-12 20:40:18 +0900

    Auth Context 구현, 유저 더미 데이터 생성 및 로그인 페이지 Context 적용
    로그인 된 사용자가 로그인 페이지, 회원 가입 페이지 접근 차단

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,19 +1,30 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { SiNaver } from 'react-icons/si';
-import { RiKakaoTalkFill } from 'react-icons/ri';
+import { RiKakaoTalkFill, RiGhostLine } from 'react-icons/ri';
 import { FcGoogle } from 'react-icons/fc';
 import { Logo } from '@/components/logo/logo';
 import Image from 'next/image';
+import { useAuth } from '@/context/auth-context';
+import { useRouter } from 'next/navigation';
 
 const SignInPage = () => {
+  const auth = useAuth();
+  const router = useRouter();
+
+  if (auth.isLoggedIn) {
+    router.push('/mentors');
+    return;
+  }
+
   return (
-    <main className='bg-gradient-to-br from-pc-500 to-orange-200 h-full w-full flex2 flex-col gap-5'>
-      <Logo className='hidden sm:block mb-5 text-8xl' />
+    <main className='bg-gradient-to-br from-pc-500 to-orange-200 h-full w-full lg:min-h-[700px] flex2 flex-col gap-5'>
+      <Logo className='hover:text-white hidden sm:block mb-5 text-8xl duration-700' />
       <Card className='bg-white flex2 w-full h-full sm:w-auto sm:h-[500px] overflow-hidden'>
-        <Logo className='text-pc-500 absolute top-[10%] sm:hidden' />
-        <section>
+        <Logo className='absolute top-[10%] sm:hidden duration-500' />
+        <section className='min-w-80'>
           <CardHeader>
             <CardTitle>로그인</CardTitle>
             <CardDescription>세가지의 소셜 로그인 중 인증</CardDescription>
@@ -21,21 +32,27 @@ const SignInPage = () => {
           <Separator className='ml-[10%] w-[80%] mb-7' />
           <CardContent className='flex flex-col gap-5'>
             <Button className='bg-green-500 hover:bg-green-500/70 py-6'>
-              <div className='w-full flex pl-5'>
+              <a href={process.env.NAVER_AUTH_URL} className='w-full flex pl-5'>
                 <SiNaver className='text-xl mr-5' />
                 <span>네이버로 로그인</span>
-              </div>
+              </a>
             </Button>
             <Button className='bg-yellow-500 hover:bg-yellow-500/70 py-6'>
-              <div className='w-full flex pl-5'>
+              <a href={process.env.KAKAO_AUTH_URL} className='w-full flex pl-5'>
                 <RiKakaoTalkFill className='text-2xl mr-5' />
                 <span>카카오로 로그인</span>
-              </div>
+              </a>
             </Button>
             <Button className='py-6'>
-              <div className='w-full flex pl-5'>
+              <a href={process.env.GOOGLE_AUTH_URL} className='w-full flex pl-5'>
                 <FcGoogle className='text-2xl mr-5' />
                 <span>구글로 로그인</span>
+              </a>
+            </Button>
+            <Button className='bg-rose-200 hover:bg-rose-200/70 text-zinc-700 py-6' onClick={auth.signIn}>
+              <div className='w-full flex pl-5'>
+                <RiGhostLine className='text-2xl mr-5' />
+                <span>테스트용 로그인</span>
               </div>
             </Button>
           </CardContent>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
-import { Noto_Sans_KR } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Noto_Sans_KR } from 'next/font/google';
+import './globals.css';
+import { AuthProvider } from '@/context/auth-context';
 
-const inter = Noto_Sans_KR({ subsets: ["latin"] });
+const inter = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Peer",
-  description: "사이드 프로젝트 상담",
+  title: 'Peer',
+  description: '사이드 프로젝트 상담',
 };
 
 export default function RootLayout({
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
+    <html lang='ko'>
+      <AuthProvider>
+        <body className={inter.className}>{children}</body>
+      </AuthProvider>
     </html>
   );
 }
