348d898a3476cd866c255d15f5692647d56d15c3
mrbonk97 <mrbonk97@gmail.com>
2024-04-12 14:03:46 +0900

    로그인 회원가입 UI 구현

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,64 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { SiNaver } from "react-icons/si";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+import { Logo } from "@/components/logo/logo";
+import Image from "next/image";
+
 const SignUpPage = () => {
-  return <main>회원 가입 페이지</main>;
+  return (
+    <main className="bg-gradient-to-br from-pc-500 to-orange-200 h-full w-full flex2 flex-col gap-5">
+      <Logo className="hidden sm:block mb-5 text-8xl" />
+      <Card className="bg-white flex2 w-full h-full sm:w-auto sm:h-[500px] overflow-hidden">
+        <Logo className="text-pc-500 absolute top-[10%] sm:hidden" />
+        <section>
+          <CardHeader>
+            <CardTitle>회원가입</CardTitle>
+            <CardDescription>
+              {/* TODO 문장 수정 필요 */}
+              모든 회원 정보는 암호화되어 저장됩니다.
+            </CardDescription>
+          </CardHeader>
+          <Separator className="ml-[10%] w-[80%] mb-7" />
+          <CardContent className="flex flex-col gap-5">
+            <Button className="bg-green-500 hover:bg-green-500/70 py-6">
+              <div className="w-full flex pl-5">
+                <SiNaver className="text-xl mr-5" />
+                <span>네이버로 로그인</span>
+              </div>
+            </Button>
+            <Button className="bg-yellow-500 hover:bg-yellow-500/70 py-6">
+              <div className="w-full flex pl-5">
+                <RiKakaoTalkFill className="text-2xl mr-5" />
+                <span>카카오로 로그인</span>
+              </div>
+            </Button>
+            <Button className="py-6">
+              <div className="w-full flex pl-5">
+                <FcGoogle className="text-2xl mr-5" />
+                <span>구글로 로그인</span>
+              </div>
+            </Button>
+          </CardContent>
+        </section>
+        <Image
+          src={"/3193464.jpg"}
+          alt="sign-in"
+          height={500}
+          width={500}
+          className="hidden lg:block ml-5 h-full border-l"
+        />
+      </Card>
+    </main>
+  );
 };
 
 export default SignUpPage;
---
+++ components/logo/logo.tsx
@@ -0,0 +1,17 @@
+import { Playfair_Display } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Playfair_Display({ subsets: ["latin"] });
+
+export const Logo = ({ className }: { className: string }) => {
+  return (
+    <h1
+      className={cn(
+        `${inter.className} text-6xl font-semi-bold tracking-wide `,
+        className
+      )}
+    >
+      Peer
+    </h1>
+  );
+};
