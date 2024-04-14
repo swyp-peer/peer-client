4629e754e6b4353a8936457b9a6d79a1bc215627
mrbonk97 <mrbonk97@gmail.com>
2024-04-14 15:38:19 +0900

    404 페이지 구현

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+  return (
+    <main className="h-full w-full flex2 flex-col gap-5">
+      <h1 className="text-pc-500 text-8xl font-bold">404.</h1>
+      <h1 className="text-pc-300 text-xl font-bold">
+        페이지를 찾을 수 없습니다.
+      </h1>
+    </main>
+  );
+};
+
+export default NotFoundPage;
