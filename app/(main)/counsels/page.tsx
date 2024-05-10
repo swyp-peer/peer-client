baea41b2ba11e5e9165690ea0fe1be744f8db3eb
topeanut <ljhh1648@gmail.com>
2024-05-10 23:00:04 +0900

    temp

--- app/(main)/counsels/page.tsx
+++ app/(main)/counsels/page.tsx
@@ -4,12 +4,12 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
 import { Card as UICard } from "@/components/ui/card";
 import React, { useState, useEffect } from "react";
-import styles from "../mentors/mainpage.module.css";
+import styles from "../counsels/page.module.css";
 import Link from "next/link";
 
 const CounselPage = () => {
   return (
-    <main>
+    <main className = {styles.}>
       나의 상담 신청 내역을 조회하는 페이지(커피챗 앱의 나의 커피챗 페이지라고
       생각하면 될듯)
     </main>
